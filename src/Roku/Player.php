4aa35dfd27a34ce1c0dd734d57526116a80e97eb
jalder <jack@jalder.com>
2014-11-18 14:46:39 -0500

    updating Roku Player

--- src/Roku/Player.php
+++ src/Roku/Player.php
@@ -1,17 +1,19 @@
 <?php
 
 namespace jalder\Upnp\Roku;
+
 use jalder\Upnp\Roku;
 use jalder\Upnp\Roku\Remote;
 
-class Player{
+class Player
+{
 
     private $remote;
     private $location;
 
-    public function __construct($location)
+    public function __construct($device)
     {
-        $this->remote = new Remote($location);
+        $this->remote = new Remote($device);
         $this->location = $this->remote->getLocation();
     }
 
@@ -20,33 +22,28 @@ class Player{
         if(is_array($video)){
 
             if(strpos($video['url'],'youtube')){
-                //var_dump($this->remote->getChannels());
                 $url = parse_url($video['url']);
                 parse_str($url['query'],$query);
                 $arguments = array(
                     'v'=>$query['v']
                 );
-                var_dump(http_build_query($arguments));
                 foreach($this->remote->getChannels() as $id=>$ch){
                     if($ch['name'] == 'YouTube'){
                         $response = $this->remote->loadChannel($id, $arguments);
-                        var_dump($response.$id);
                     }
                 }
-
-                die();
             }
-
-            $arguments = array(
-                'url'=>$video['url'],
-                'StreamFormat'=>$video['format'],
-                'srt'=>$video['subtitle_url'],
-                'title'=>$video['title'],
+            else{
+                $arguments = array(
+                    'url'=>$video['url'],
+                    'StreamFormat'=>$video['format'],
+                    'srt'=>$video['subtitle_url'],
+                    'title'=>$video['title'],
                
-            );
-            $response = $this->remote->loadChannel('dev',$arguments);
-            return true;
+                );
+                $response = $this->remote->loadChannel('dev',$arguments);
+            }    
+            return $response;
         }
     }
-
 }
